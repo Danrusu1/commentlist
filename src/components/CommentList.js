57a17d15cf9fc95f9a1f0b0b02b49baaab35ddca
rusud <d.rusu@plan-net.com>
2021-12-06 18:23:00 +0200

    make CommentList sparkle

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -24,7 +24,7 @@ import PropTypes from 'prop-types';
     rgba(248, 248, 254, 0.95),
     rgba(250, 250, 250, 0.95)
    );
-   border-radius: 48px;
+  //  border-radius: 48px;
  `;
 
  const AvatarDiv = styled.div`
